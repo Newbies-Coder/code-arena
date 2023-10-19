5e7aebd69297d4f79ff4fb9b1742c5fc08e78334
Shar <66898063+VN-Shar@users.noreply.github.com>
2023-10-19 15:18:50 +0700

    logging init

--- backend/src/library/logging.ts
+++ backend/src/library/logging.ts
@@ -4,23 +4,27 @@ import { v4 as uuidv4 } from 'uuid'
 import fs from 'fs'
 import path from 'path'
 import fsPromises from 'fs/promises'
+import { config } from '../config/env.config'
 
 class Logging {
   public static logEvents = async (message: string, logFileName: string) => {
-    const date = moment(new Date()).format('DD-MM-YYYY\\tHH:mm:ssSSS')
-    const logItem = `${date}\t${uuidv4()}\t${message}\n`
+    const now = moment(new Date()).format('DD-MM-YYYY\\tHH:mm:ssSSS')
+    const date = moment(new Date()).format('DD-MM-YYYY')
+    const logItem = `${uuidv4()}\t${now}\t${message}\n`
+
     try {
-      if (!fs.existsSync(path.join(__dirname, '../', 'logs'))) {
-        await fsPromises.mkdir(path.join(__dirname, '../', 'logs'))
+      if (!fs.existsSync(path.join(config.server.log_path, date))) {
+        await fsPromises.mkdir(path.join(config.server.log_path, date))
       }
-      await fsPromises.appendFile(path.join(__dirname, '../', 'logs', logFileName), logItem)
+      await fsPromises.appendFile(path.join(config.server.log_path, date, logFileName), logItem)
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
   }
+
   public static logger = (req: Request, res: Response, next: NextFunction) => {
-    this.logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
-    console.log(`${req.method}\t${req.path}`)
+    let message = `${req.method}\t${req.url}\t${res.statusCode}\t${req.headers.origin}`
+    this.logEvents(message, 'reqLog.log')
     next()
   }
 }
