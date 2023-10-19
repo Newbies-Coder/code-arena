d61f52335862f68048e4aab145a1d42a8c2a865d
DuongNam <duongquocnam224400@gmail.com>
2023-10-19 10:32:54 +0700

    setup server local

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const HOST = process.env.HOST ?? 'localhost'
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000
+
 export const config = {
   client: {},
-  server: {},
+  server: {
+    port: PORT,
+    host: HOST
+  },
   database: {},
   jwt_token: {}
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,57 @@
-import express from 'express'
-
-const host = process.env.HOST ?? 'localhost'
-const port = process.env.PORT ? Number(process.env.PORT) : 8080
+import express, { Request, Response, NextFunction } from 'express'
+import { config } from './config/env.config'
+import { createServer } from 'http'
+import helmet from 'helmet'
+import compression from 'compression'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+const httpServer = createServer(app)
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ['*'],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'https://fonts.googleapis.com/css',
+          'https://use.fontawesome.com/releases/v5.12.0/css/all.css'
+        ],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: []
+      }
+    }
+  })
+)
+app.use(compression())
+app.use(cors())
+app.use(morgan('dev'))
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cookieParser())
+app.use(express.static('.'))
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' })
 })
 
-app.listen(port, host, () => {
-  console.log(`Server running on ${port}`)
+httpServer.listen(config.server.port, config.server.host, () => {
+  console.log(`🚀 Server Running On Port ${config.server.port}`)
 })
