6bdad2788669b3257ebdabfda4878878ddf58139
DuongNam <duongquocnam224400@gmail.com>
2023-10-19 16:05:05 +0700

    Add password and name user-logs mongodb

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -4,6 +4,8 @@ dotenv.config()
 
 const HOST = process.env.HOST ?? 'localhost'
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000
+const DB_PASS_USER_LOGS = process.env.DB_PASS_USER_LOGS
+const DB_USER_LOG = process.env.DB_USER_LOG
 const LOG_PATH = '../backend/src/logs'
 
 export const config = {
@@ -13,6 +15,9 @@ export const config = {
     host: HOST,
     log_path: LOG_PATH
   },
-  database: {},
+  database: {
+    log_name: DB_USER_LOG,
+    log_pass: DB_PASS_USER_LOGS
+  },
   jwt_token: {}
 }
