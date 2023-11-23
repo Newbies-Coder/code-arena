1638d5c55cf07b18b240d9cdb3493b8f7e0a7d5e
DuongNam <duongquocnam224400@gmail.com>
2023-11-23 14:34:10 +0700

    feat: setup redux-toolkit

---
+++ frontend/src/@types/user.ts
@@ -0,0 +1,10 @@
+export interface userType {
+  email?: string
+  passWord?: string
+  accessToken?: string
+  refreshToken?: string
+}
+
+export interface userState {
+  userLogin: userType
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,18 @@
 import { Suspense } from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { publicRoute } from './routes'
 import Loading from './components/Loading'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          {publicRoute.map((route, idx) => {
-            const Page = route.component
-            return <Route key={idx} path={route.path} element={<Page />}></Route>
-          })}
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        {publicRoute.map((route, idx) => {
+          const Page = route.component
+          return <Route key={idx} path={route.path} element={<Page />}></Route>
+        })}
+      </Routes>
+    </Suspense>
   )
 }
 
