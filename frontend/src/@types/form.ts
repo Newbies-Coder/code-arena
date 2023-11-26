b28277e9270171b8c07442608fcbbb1cbc708293
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2023-11-26 15:29:17 +0700

    perf: render socialmedia button

--- frontend/src/@types/form.ts
+++ frontend/src/@types/form.ts
@@ -12,3 +12,10 @@ export type FormItemPropsType = {
   Icon: React.ElementType
   inputType?: string
 }
+
+export type SocialMediaType = {
+  key: string
+  url?: string
+  alt?: string
+  icon?: React.ElementType
+}
