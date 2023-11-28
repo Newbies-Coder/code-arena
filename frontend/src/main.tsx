5d571cc0c6654f21383eb62dde3c60b7b7a83525
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2023-11-28 21:49:35 +0700

    refactor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,11 @@ import * as ReactDOM from 'react-dom/client'
 import '@/index.scss'
 import 'normalize.css'
 import 'antd/dist/reset.css'
-import App from '@/App'
 import { unstable_HistoryRouter as HistoryBrowserRouter } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { Provider } from 'react-redux'
 import { store } from '@redux/config'
 import Verification from './container/Auth/pages/Verification'
-import PasswordCongratulation from './components/PasswordCongratulation'
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 export const history: any = createBrowserHistory()
 
