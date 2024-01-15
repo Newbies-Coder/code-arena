e990cc4bd6d8ceae9afd8a00b08506cee9599129
dattv23 <datvan635@gmail.com>
2024-01-15 23:16:11 +0700

    fix: expired time otp

--- backend/src/models/schemas/Otps.schema.ts
+++ backend/src/models/schemas/Otps.schema.ts
@@ -21,7 +21,7 @@ export default class OTP {
     this._id = item._id
     this.email = item.email
     this.otp = item.otp
-    this.expiredIn = item.expiredIn || new Date(Date.now() + 5 * 60000)
+    this.expiredIn = new Date(Date.now() + 5 * 60000)
     this.created_at = item.created_at || new Date()
     this.updated_at = item.updated_at || null
   }
