8a8bdbdc4d5d2feb3809c06e8538a1c2bed0b94f
DuongNam <duongquocnam224400@gmail.com>
2024-01-15 23:19:15 +0700

    fix: config datetime expired

--- backend/src/services/otp.service.ts
+++ backend/src/services/otp.service.ts
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { GenerateOTPResult } from '~/@types/auth.type'
 import OTP from '~/models/schemas/Otps.schema'
 import { databaseService } from '~/services/connectDB.service'
@@ -15,10 +16,22 @@ class OTPService {
       otp: hashOTP(otpCode),
       expiredIn: new Date()
     })
-
     await databaseService.otps.insertOne(otp)
     const result: GenerateOTPResult = { code: otpCode, email: email }
     return result
+  }
+  async isOtpExpired(otp: string): Promise<Boolean> {
+    // Retrieve the OTP document by ID or some unique identifier
+    console.log(hashOTP(otp))
+    const otpDocument = await databaseService.otps.findOne({ otp: hashOTP(otp) })
+
+    if (!otpDocument) {
+      throw new Error('OTP not found')
+    }
+    const otpExpirationTime = otpDocument.expiredIn // Assuming this is a Date object
+    const currentTime = moment() // Current time
+    // Compare the timestamps
+    return currentTime.isAfter(otpExpirationTime) // Returns true if OTP is expired
   }
 }
 
