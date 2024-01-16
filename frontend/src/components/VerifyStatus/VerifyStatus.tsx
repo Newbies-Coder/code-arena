3c5c4b748079ee2d33eba74c5b03670d79dfa970
dattv23 <datvan635@gmail.com>
2024-01-16 14:47:32 +0700

    refactor: component verify status

--- frontend/src/components/VerifyStatus/VerifyStatus.tsx
+++ frontend/src/components/VerifyStatus/VerifyStatus.tsx
@@ -12,7 +12,10 @@ const StatusColor = {
 const VerifyStatus = (props: VerifyStatusProps) => {
   return (
     <div className="flex items-center justify-start gap-2">
-      <div className="w-2 h-2 rounded-full" style={{ background: StatusColor[props.name] }}></div>
+      <div
+        className="w-2 h-2 rounded-full"
+        style={{ background: StatusColor[props.name as keyof typeof StatusColor] }}
+      ></div>
       <div className="text-sm font-normal font-['Poppins'] leading-normal">{props.name}</div>
     </div>
   )
