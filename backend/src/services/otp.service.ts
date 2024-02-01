54a206f3e051fe4aef6db12bcefc696c935c0e52
DuongNam <duongquocnam224400@gmail.com>
2024-02-01 22:56:55 +0700

    fix: implement error handling send-otp process

--- backend/src/services/otp.service.ts
+++ backend/src/services/otp.service.ts
@@ -1,5 +1,8 @@
+import { StatusCodes } from 'http-status-codes'
 import moment from 'moment'
 import { GenerateOTPResult } from '~/@types/auth.type'
+import { DEV_ERRORS_MESSAGES } from '~/constants/message'
+import { ErrorWithStatus } from '~/models/errors/Errors.schema'
 import OTP from '~/models/schemas/Otps.schema'
 import { databaseService } from '~/services/connectDB.service'
 import { generateOTPCode, hashOTP } from '~/utils/crypto'
@@ -9,15 +12,25 @@ class OTPService {
     return await databaseService.otps.findOne<OTP>({ otp: hashOTP(otp) })
   }
 
-  async generateOTP(email: string) {
-    const otpCode = generateOTPCode()
-    const otp = new OTP({
-      email,
-      otp: hashOTP(otpCode)
-    })
-    await databaseService.otps.insertOne(otp)
-    const result: GenerateOTPResult = { code: otpCode, email: email }
-    return result
+  async generateOTP(email: string): Promise<GenerateOTPResult> {
+    try {
+      const otpCode = generateOTPCode()
+      let OTP_LIFETIME = 5 * 60 * 1000
+      const otp = new OTP({
+        email,
+        otp: hashOTP(otpCode),
+        expiredIn: new Date(Date.now() + OTP_LIFETIME) // Expires in 5 minutes
+      })
+
+      await databaseService.otps.deleteMany({ email: email })
+      await databaseService.otps.insertOne(otp)
+      return { code: otpCode, email }
+    } catch (error) {
+      throw new ErrorWithStatus({
+        statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        message: error.message || DEV_ERRORS_MESSAGES.GENERATED_OTP
+      })
+    }
   }
 
   async isOtpExpired(otp: string): Promise<Boolean> {
