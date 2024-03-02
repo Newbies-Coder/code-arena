38092f73ec661c0923a7628dc51f84ef065f07f4
dattv23 <datvan635@gmail.com>
2024-03-02 15:12:46 +0700

    feat: add account

--- frontend/src/container/Admin/User/pages/AddAccount/AddAccount.tsx
+++ frontend/src/container/Admin/User/pages/AddAccount/AddAccount.tsx
@@ -1,261 +1,251 @@
-import { Alert, Button, Form, Input } from 'antd'
+import { AccountType, UserRole } from '@/@types/admin.type'
+import { Alert, Button, Form, Radio, Select } from 'antd'
+import FormItem, { InputProps } from '../../components/FormItem/FormItem'
+import { regexDateOfBirth, regexPasswordPattern } from '@/utils/regex'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import requestApi from '@/utils/interceptors'
+import { handleApiError } from '@/utils/handleApiError'
 
 const AddAccount = () => {
+  const navigate = useNavigate()
+
+  // Define an array of input properties for the form
+  const formInputProperties: InputProps[] = [
+    {
+      name: 'fullName',
+      label: 'Full Name',
+      rules: [
+        {
+          max: 50,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message={`Length max is 50 characters!`}
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'username',
+      label: 'Username',
+      rules: [
+        {
+          max: 20,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message={`Length max is 20 characters!`}
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'email',
+      label: 'Email Address',
+      rules: [
+        {
+          type: 'email',
+          message: (
+            <Alert className="bg-transparent text-base text-red-700" message={`Email not valid!`} banner type="error" />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'phone',
+      label: 'Phone Number',
+      rules: [
+        {
+          pattern: /^0\d{9}$/g,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message="Phone number not valid!"
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      rules: [
+        {
+          // eslint-disable-next-line no-useless-escape
+          pattern: /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message={`Password not valid!`}
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'confirm_password',
+      label: 'Confirm Password',
+      rules: [
+        {
+          pattern: regexPasswordPattern,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message={`Confirm password not valid!`}
+              banner
+              type="error"
+            />
+          ),
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+              return Promise.resolve()
+            }
+            return Promise.reject(new Error('The new password that you entered do not match!'))
+          },
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message={`The new password that you entered do not match!`}
+              banner
+              type="error"
+            />
+          ),
+        }),
+      ],
+    },
+    {
+      name: 'role',
+      label: 'Role',
+      children: (
+        <Select className="h-12 text-white text-md border-[2px] rounded-lg">
+          <Select.Option value={UserRole.User}>{UserRole.User}</Select.Option>
+          <Select.Option value={UserRole.Moderator}>{UserRole.Moderator}</Select.Option>
+          <Select.Option value={UserRole.Admin}>{UserRole.Admin}</Select.Option>
+        </Select>
+      ),
+    },
+    {
+      name: 'date_of_birth',
+      label: 'Date of Birth',
+      placeholder: 'yyyy-mm-dd',
+      rules: [
+        {
+          pattern: regexDateOfBirth,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message="Date of Birth not valid!"
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'address',
+      label: 'Address',
+      rules: [
+        {
+          pattern: /^.{10,255}$/g,
+          message: (
+            <Alert
+              className="bg-transparent text-base text-red-700"
+              message="Address is not valid"
+              banner
+              type="error"
+            />
+          ),
+        },
+      ],
+    },
+    {
+      name: 'gender',
+      label: 'Gender',
+      children: (
+        <Radio.Group className="mt-5 text-white ml-5">
+          <Radio value="Male" className="text-white text-xl" checked={true}>
+            Male
+          </Radio>
+          <Radio value="Female" className="text-white text-xl">
+            Female
+          </Radio>
+        </Radio.Group>
+      ),
+    },
+  ]
+
+  // Defining an asynchronous function to handle form submission
+  const handleFormSubmit = async (values: AccountType) => {
+    const { fullName, username, email, phone, password, confirm_password, role, date_of_birth, address, gender } =
+      values
+    const loadingToast = toast.loading('Creating Account')
+    try {
+      const res = await requestApi(`auth/create-user`, 'post', {
+        fullName,
+        username,
+        email,
+        phone,
+        password,
+        confirm_password,
+        role,
+        date_of_birth,
+        address,
+        gender,
+      })
+      const { message } = res.data
+      toast.update(loadingToast, {
+        render: message,
+        type: 'success',
+        isLoading: false,
+        autoClose: 3000,
+      })
+      navigate('/admin/user')
+    } catch (error) {
+      toast.dismiss(loadingToast)
+      handleApiError(error)
+    } finally {
+      toast.clearWaitingQueue()
+    }
+  }
+
   return (
     <div className="px-10 py-5">
       <h2 className="text-orange-400 text-3xl font-medium font-['Poppins'] leading-9 mb-12">Account information</h2>
-      <Form name="basic" initialValues={{ remember: true }} className="w-full flex flex-col items-center relative mt-4">
-        <div className="flex flex-col w-full lg:flex-row lg:gap-2">
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">Full name</h3>
-            <Form.Item
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your name"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="Alex Jordan"
-              />
-            </Form.Item>
-          </div>
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Username
-            </h3>
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your username"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="AlexJordan01"
-              />
-            </Form.Item>
-          </div>
-        </div>
-        <div className="flex flex-col w-full lg:flex-row lg:gap-2">
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Phone number
-            </h3>
-            <Form.Item
-              name="phone"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your phone"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                type="number"
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="0123456789"
-              />
-            </Form.Item>
-          </div>
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Email
-            </h3>
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your email"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="johnpham@gamil.com"
-              />
-            </Form.Item>
-          </div>
-        </div>
-        <div className="flex flex-col w-full lg:flex-row lg:gap-2">
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Password
-            </h3>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Invalid password"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your password"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                type="password"
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="Alex@123"
-              />
-            </Form.Item>
-          </div>
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Confirm password
-            </h3>
-            <Form.Item
-              name="confirm-password"
-              rules={[
-                {
-                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Invalid confirm-password"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your password"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                type="password"
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="Alex@123"
-              />
-            </Form.Item>
-          </div>
-        </div>
-        <div className="flex flex-col w-full lg:flex-row lg:gap-2">
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Role
-            </h3>
-            <Form.Item
-              name="role"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your role"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="admin"
-              />
-            </Form.Item>
-          </div>
-          <div className="w-full relative">
-            <h3 className="absolute -top-2 left-3 lg:-top-2 lg:left-3 px-2 mb-0 text-white bg-[#001529] z-10 rounded-md">
-              Verify
-            </h3>
-            <Form.Item
-              name="status"
-              rules={[
-                {
-                  required: true,
-                  message: (
-                    <Alert
-                      className="bg-transparent text-base text-red-700"
-                      message="Please input your status"
-                      banner
-                      type="error"
-                    />
-                  ),
-                },
-              ]}
-              className="border-2 rounded-lg border-white w-full mb-10 flex flex-col"
-            >
-              <Input
-                className="h-12 bg-transparent border-none text-white text-xl focus:shadow-none focus:border-none focus:outline-none focus-visible:shadow-none focus-visible:border-none focus-visible:outline-none placeholder:text-[#7b7878]"
-                placeholder="Active"
-              />
-            </Form.Item>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 w-full lg:flex-row lg:gap-2 lg:justify-center">
+      <Form
+        name="basic"
+        className="w-full mt-4 text-white grid gap-4 grid-cols-1 lg:grid-cols-2"
+        onFinish={handleFormSubmit}
+      >
+        {formInputProperties.map((item, index) => (
+          <FormItem
+            key={index}
+            name={item.name}
+            label={item.label}
+            children={item.children}
+            rules={item.rules}
+            required={item.required}
+          />
+        ))}
+        <div className="flex flex-col gap-2 w-full lg:flex-row lg:gap-2 lg:justify-center col-span-full">
           <Button
             type="primary"
-            className="w-full lg:w-[147px] h-12 px-6 py-2.5 bg-[#FFF2E7] rounded-lg shadow border border-[#7302E8] justify-center items-center gap-2 flex text-[#7302E8] text-base font-popins"
+            className="w-full lg:w-[147px] h-12 px-6 py-2.5 bg-[#FFF2E7] rounded-lg shadow border border-[#7302E8] justify-center items-center gap-2 flex text-[#7302E8] text-base font-popins disabled:text-white"
+            disabled={false}
+            onClick={() => navigate(-1)}
           >
             Cancel
           </Button>
@@ -263,7 +253,7 @@ const AddAccount = () => {
             <Button
               type="primary"
               htmlType="submit"
-              className="w-full lg:w-[147px] h-12 px-6 py-2.5 bg-[#7302E8] rounded-lg shadow border border-[#7302E8] justify-center items-center gap-2 flex text-[#FFF2E7] text-base font-popins"
+              className="w-full lg:w-[147px] h-12 px-6 py-2.5 bg-[#7302E8] rounded-lg shadow border border-[#7302E8] justify-center items-center gap-2 flex text-[#FFF2E7] text-base font-popins disabled:text-white"
             >
               Add
             </Button>
